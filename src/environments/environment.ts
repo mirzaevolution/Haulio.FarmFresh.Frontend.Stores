c101e265cecbefa48c37a5d52ae5232fa2c0599a
Mirza Ghulam Rasyid <mirza.rasyid@kobkiat-it.com>
2022-01-25 00:46:47 +0700

    Added env data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  idp:{
+    baseAddress: 'https://localhost:44310',
+    clientId: 'FarmFresh.Frontend',
+    clientSecret: 'c829df0c-bf98-44c5-8c92-fbeab7039ac4',
+    scope: 'openid profile email offline_access role store_services customer_services',
+    grantType: 'code',
+  },
+  apiGateway: {
+    baseAddress: 'https://localhost:44320'
+  },
+  storesApiPath: {
+     getCategories: '/api/v1/stores/categories',
+     getOrderHistories: '/api/v1/stores/orderhistories',
+     getProductById: '/api/v1/stores/products/',
+     getAllProducts: '/api/v1/stores/products',
+     postProduct: '/api/v1/stores/products',
+     putProduct: '/api/v1/stores/products',
+     deleteProductById : '/api/v1/stores/products/'
+  }
 };
 
 /*
